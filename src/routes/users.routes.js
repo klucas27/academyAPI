4b1798e88f39704b1867a28a77cf5fb547da27d4
Arch_lucas <lucas27ro@gmail.com>
2025-06-14 20:00:34 -0300

    fix: correct order of parameters in userGet endpoint and improve response messages

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,9 +2,24 @@ import express from 'express';
 
 const router = express.Router();
 
+// router.use(express.json())
+
 router.get('/test', (req, res) => {
   res.json({ mensagem: 'API do Academy está online!, endpoit: users' });
 });
 
+router.post('/userGet', (res, req) => {
+    const {user, passwd} = req.body
 
+    if (user === "test" && passwd === "123") {
+        res.json({
+            sucesso: true, mensagem: "Login Aprovado!"
+        })
+    } else {
+        res.status(401).json({
+            sucesso: false, mensagem: "Usuario ou senha incorreto!"
+        })
+    }
+
+})
 export default router;
