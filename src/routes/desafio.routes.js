0642b26f7eea6ada281b3178ff349763e2d5561b
Arch_lucas <lucas27ro@gmail.com>
2025-06-14 18:40:05 -0300

    fix: add file extension to import statement in desafio.routes.js

--- src/routes/desafio.routes.js
+++ src/routes/desafio.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { gerarPergunta } from '../services/openai.service';
+import { gerarPergunta } from '../services/openai.service.js';
 
 const router = express.Router();
 
