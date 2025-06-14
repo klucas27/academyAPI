33548647d663274fde5cc2fa0d806d0de4ada2af
Arch_lucas <lucas27ro@gmail.com>
2025-06-14 19:50:29 -0300

    fix: correct response message in test endpoint of users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/test', (req, res) => {
-  res.json({ mensagem: 'API do Academy está online!' });
+  res.json({ mensagem: 'API do Academy está online!, endpoit: users' });
 });
 
 
