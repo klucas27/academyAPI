5c928cd2fe2aa21395f57e991224fb2f5875ed99
Arch_lucas <lucas27ro@gmail.com>
2025-06-14 19:48:54 -0300

    fix: add missing semicolons and improve CORS comment; create users.routes.js with test endpoint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // src/index.js
 
-import './loadEnv.js'
+import './loadEnv.js';
 
 import express from 'express';
-import cors  from 'cors'
+import cors  from 'cors';
 import desafioRoutes from './routes/desafio.routes.js';
+
+import usersInfos from './routes/users.routes.js';
 
 
 // console.log("Chave OpenAI:", process.env.OPENAI_API_KEY);  // Adicione isso para verificar se a chave está sendo carregada
@@ -20,10 +22,12 @@ const PORT = process.env.PORT || 3006;
 //   credentials: false
 // }));
 
-app.use(cors())
+app.use(cors())   // Liberação geral do Cors * SOMENTE PARA TESTES
+
 app.use(express.json());
 
 app.use('/api', desafioRoutes);
+app.use('/users', usersInfos);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
