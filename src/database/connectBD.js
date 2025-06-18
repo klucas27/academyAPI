83e3baa87da3824272b1c435a243351c62191b21
Arch_lucas <lucas27ro@gmail.com>
2025-06-18 00:51:35 -0300

    fix: switch from mysql.createConnection to mysql.createPool for improved database connection management

--- src/database/connectBD.js
+++ src/database/connectBD.js
@@ -4,21 +4,24 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.HOST_BD,
   user: process.env.USER_BD,
   password: process.env.PASSWORD_BD,
-  database: process.env.DATABASE_BD
+  database: process.env.DATABASE_BD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 
 // Conectar ao banco
-connection.connect((err) => {
+pool.connect((err) => {
   if (err) {
     console.error('Erro na conexão:', err.stack);
     return;
   }
-  console.log('Conectado ao MySQL como ID', connection.threadId);
+  console.log('Conectado ao MySQL como ID', pool.threadId);
 });
 
-export default connection;
+export default pool;
